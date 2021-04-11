d79137a27023c90c1f41b402fb89465f25345777
Nikita(PC) <nikita.verba04@mail.ru>
2021-04-11 19:08:33 +0300

    added ui; removed possibility to enter conference with conference id

--- zoom_autoconnect.py
+++ zoom_autoconnect.py
@@ -1,70 +1,81 @@
 import os
-import time
 import datetime
-import pyautogui
 import webbrowser
+import tkinter as tk
 
-# path to zoom.exe
 PATH = r"C:\Users\NikOne\AppData\Roaming\Zoom\bin\Zoom.exe"
-ENTER_CONF_PHOTO = r"enter_the_conference.png"
-ENTER_PHOTO = r"enter.png"
-pyautogui.PAUSE = 0.5
 
-''' 
-    enter links or id and passwords like this: 
-    [["connect_link", "year-month-day-hours-minutes"], ["connect_id", "password", "year-month-day-hours-minutes"]]
-    example: [[r"https://zoom.com/1073729/", "2021-08-15-19-30"], ["2s5e8x25s", "52247167", "2021-08-15-19-30"]]
-'''
+window = tk.Tk()
+
+window.rowconfigure(5, minsize=50, weight=1)
+window.columnconfigure([0, 1, 2], minsize=50, weight=1)
+
+conf_fields = {}
 conferences = []
+number = [0]
 
 
-def enter_the_conference(conf_id, conf_pass):
-    if not conf_id.startswith("https"):
-        pos = pyautogui.locateOnScreen(ENTER_CONF_PHOTO)
-        x = pos[0] + pos[2] / 2
-        y = pos[1] + pos[3] / 2
-        pyautogui.click(x=x, y=y, button='left')
-        time.sleep(3)
-        pyautogui.typewrite(conf_id)
-        pos = pyautogui.locateOnScreen(ENTER_PHOTO)
-        x = pos[0] + pos[2] / 2
-        y = pos[1] + pos[3] / 2
-        pyautogui.click(x=x, y=y, button='left')
-        time.sleep(1)
-        pyautogui.typewrite(conf_pass)
-        pos = pyautogui.locateOnScreen(ENTER_PHOTO)
-        x = pos[0] + pos[2] / 2
-        y = pos[1] + pos[3] / 2
-        pyautogui.click(x=x, y=y, button='left')
+# adding conference fields to the application window
+def add_conference():
+    number[0] += 1
+    conf_fields[number[0]] = []
+    link = tk.Entry(master=window)
+    link.grid(row=number[0], column=0, sticky="nsew")
+    conf_fields[number[0]].append(link)
+    time = tk.Entry(master=window)
+    time.grid(row=number[0], column=1)
+    conf_fields[number[0]].append(time)
+
+
+# entering conference
+def enter_conf(link, path):
+    os.system(r"taskkill /f /im Zoom.exe")
+    os.startfile(path)
+    window.after(8)
+    webbrowser.open(link)
+
+
+# start application
+def start():
+    if start_btn["text"] == "Start":
+        start_btn["text"] = "Stop"
+        for fields in conf_fields:
+            conferences.append([conf_fields[fields][0].get(), conf_fields[fields][1].get()])
+
+        # sort conferences by date
+        def sort_by_date(conf):
+            if conf[0].startswith("http") or conf[0].startswith("www"):
+                date = conf[1]
+            else:
+                date = conf[2]
+            year, month, day, hour, minute = date.split("-")
+            return datetime.datetime(int(year), int(month), int(day), int(hour), int(minute)).timestamp()
+
+        conferences.sort(key=sort_by_date)
+
+        for conference in conferences:
+            if conference[0].startswith("http") or conference[0].startswith("www"):
+                _date = conference[1]
+            else:
+                _date = conference[2]
+            _year, _month, _day, _hour, _minute = _date.split("-")
+            time_delta = datetime.datetime(int(_year), int(_month), int(_day), int(_hour), int(_minute)).timestamp() - \
+                         datetime.datetime.today().timestamp()
+            if time_delta > 0:
+                print("Sleeping for " + str(time_delta) + "sec")
+                # time.sleep(time_delta)
+                window.after(int(time_delta * 1000), enter_conf, conference[0], PATH)
+            else:
+                continue
+
     else:
-        webbrowser.open(conference[0])
+        exit()
 
 
-if __name__ == "__main__":
-    def sort_by_date(conf):
-        if conf[0].startswith("https"):
-            date = conf[1]
-        else:
-            date = conf[2]
-        year, month, day, hour, minute = date.split("-")
-        return datetime.datetime(int(year), int(month), int(day), int(hour), int(minute)).timestamp()
+start_btn = tk.Button(master=window, text="Start", command=start)
+start_btn.grid(row=0, column=0, sticky="nsew")
 
-    conferences.sort(key=sort_by_date)
+btn_add = tk.Button(master=window, text="Add conference", command=add_conference)
+btn_add.grid(row=0, column=1, sticky="nsew")
 
-    for conference in conferences:
-        if conference[0].startswith("https"):
-            _date = conference[1]
-        else:
-            _date = conference[2]
-        _year, _month, _day, _hour, _minute = _date.split("-")
-        time_delta = datetime.datetime(int(_year), int(_month), int(_day), int(_hour), int(_minute)).timestamp() - \
-            datetime.datetime.today().timestamp()
-        if time_delta > 0:
-            print("Sleeping for " + str(time_delta) + "sec")
-            time.sleep(time_delta)
-        else:
-            continue
-        os.system(r"taskkill /f /im Zoom.exe")
-        os.startfile(PATH)
-        time.sleep(8)
-        enter_the_conference(conference[0], conference[1])
+window.mainloop()
